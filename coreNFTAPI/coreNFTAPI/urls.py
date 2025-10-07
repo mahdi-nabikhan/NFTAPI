1239fa3442ad8ae3387cc50afc05d4faa3c3a166
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-10-07 15:49:40 +0330

    add static urls in urls core and static conf in settings.py

--- coreNFTAPI/coreNFTAPI/urls.py
+++ coreNFTAPI/coreNFTAPI/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
